d07204c3a2eef5d0fa2e6954723561d07b59b067
Toby Zerner <toby.zerner@gmail.com>
2014-05-17 13:04:47 +0930

    add readme + translations

--- views/settings.php
+++ views/settings.php
@@ -13,9 +13,9 @@ $form = $data["wordFilterSettingsForm"];
 <ul class='form'>
 
 <li>
-<label>Word filters</label>
+<label><?php echo T("Word filters"); ?></label>
 <?php echo $form->input("filters", "textarea", array("style" => "height:200px; width:350px")); ?>
-<small>Enter each word on a new line. Optionally specify a replacement after a vertical bar (|) character; otherwise, the word will be replaced with asterisks (*). Words are case-insensitive. Regular expressions are allowed.</small>
+<small><?php echo T("message.wordFilterInstructions", "Enter each word on a new line. Optionally specify a replacement after a vertical bar (|) character; otherwise, the word will be replaced with asterisks (*). Words are case-insensitive. Regular expressions are allowed."); ?></small>
 </li>
 
 </ul>
